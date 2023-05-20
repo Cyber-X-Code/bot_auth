af7b808bf359b745419cc186e881cf160da1d913
Cyber-X-Corp <cyber.x@list.ru>
2023-05-20 23:09:42 +0300

    Добавлен Readme файл с подробным описанием работы бота

--- bot_auth.py
+++ bot_auth.py
@@ -5,7 +5,7 @@ import time
 import requests
 
 # Впишите токен вашего бота (в правильном формате)
-TOKEN = ''
+TOKEN = 'Ваш_токен'
 
 # Список портов, которые будем прослушивать
 PORTS = [20, 21, 22, 23, 5900, 5901]
@@ -13,7 +13,7 @@ PORTS = [20, 21, 22, 23, 5900, 5901]
 bot = telebot.TeleBot(TOKEN)
 
 # Получаем имя пользователя
-username = 'Ваш_токен'
+username = ''
 users = psutil.users()
 if users: # Проверяем, что список users не пустой
     username = users[0].name
